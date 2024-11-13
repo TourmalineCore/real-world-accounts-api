17c417fa7e86477b8442a364393f97c37a891d96
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2024-11-13 11:31:38 +0500

    feat: remove old permissions

---
+++ DataAccess/Migrations/20241113063102_RemoveOldPermissions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveOldPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Permissions",
+                value: new[] { "ViewAccounts", "ManageAccounts", "ViewRoles", "ManageRoles", "CanManageTenants", "IsTenantsHardDeleteAllowed", "GuestActionsAllowed" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Permissions",
+                value: new[] { "ViewPersonalProfile", "ViewContacts", "ViewSalaryAndDocumentsData", "EditFullEmployeesData", "AccessAnalyticalForecastsPage", "ViewAccounts", "ManageAccounts", "ViewRoles", "ManageRoles", "CanRequestCompensations", "CanManageCompensations", "CanManageDocuments", "CanManageTenants", "IsTenantsHardDeleteAllowed" });
+        }
+    }
+}
